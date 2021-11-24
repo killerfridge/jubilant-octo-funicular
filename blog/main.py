13c57b9fe0bc361fe8ff3ff6d63b0b50924df85d
danielrussell-brain <daniel.rb@hotmail.co.uk>
2021-11-24 10:23:02 +0000

    Deleted additional routing from main.py that should now be covered in auth.py

--- blog/main.py
+++ blog/main.py
@@ -1,10 +1,6 @@
 from fastapi import FastAPI, status, Response, HTTPException, Depends
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from . import models
 from .database import engine, get_db
-from .schemas import PostCreate, Post, User, UserCreate, UserValidate, UserAll
-from sqlalchemy.orm import Session
-from typing import List
 from blog.routers import post, user, auth
 
 models.Base.metadata.create_all(bind=engine)
@@ -39,14 +35,3 @@ app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
 
-
-@app.post('/token', tags=['Security'])
-async def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
-    user = db.query(models.User).filter(models.User.username == form_data.username).first()
-    if not user:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Username or password incorrect")
-
-    if not user.check_password(form_data.password):
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Username or password incorrect")
-
-    return {"access_token": user.username, 'token_type': 'bearer'}
