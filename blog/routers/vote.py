7b4b06a09e4f32f991c1e7537e152a9354cb0a4b
danielrussell-brain <daniel.rb@hotmail.co.uk>
2021-11-25 13:38:06 +0000

    Fixed votes so that it returns a 404 error if the post does not exist

--- blog/routers/vote.py
+++ blog/routers/vote.py
@@ -12,6 +12,9 @@ router = APIRouter(
 @router.post('/', status_code=status.HTTP_201_CREATED)
 def vote(vote: schemas.Vote, user: schemas.UserAll = Depends(get_current_user), db: Session = Depends(database.get_db)):
 
+    if not db.query(models.Post).filter(models.Post.id == vote.post_id).first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id {vote.post_id} does not exist")
+
     vote_query = db.query(models.Vote).filter(models.Vote.post_id == vote.post_id, models.Vote.user_id == user.id)
     found_vote = vote_query.first()
 
